1dca8231ec3ae32c408688f19b2b29125b29f5f8
techub <tech4phil@outlook.com>
2022-11-21 13:41:10 +0100

    first commit

---
+++ src/main/java/com/semicolonafrica/locationmanagementapi/LocationManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.semicolonafrica.locationmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationManagementApiApplication.class, args);
+    }
+
+}
